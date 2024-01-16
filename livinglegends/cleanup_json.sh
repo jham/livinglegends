0db59dde7ee831822c1ef0dfa5d9ba1db9b78511
Jeff Hamilton <jham@thewesternshore.com>
2024-01-16 12:11:57 -0500

    Switch to parsing CSV with a python script
    The switch also converts the empty "new" and "event" fields form empty string to null

--- livinglegends/cleanup_json.sh
+++ livinglegends/cleanup_json.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sed -i '' 's/points": "\([0-9]*\)"/points": \1/' $1
+cat $1 | python3 -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)], indent = 4))' > $2
+sed -i '' 's/points": "\([0-9]*\)"/points": \1/' $2
